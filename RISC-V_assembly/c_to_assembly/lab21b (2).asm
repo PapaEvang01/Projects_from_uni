15ecd11067d990c512bb94420f4c7574fcf4a0fc
EVANGELOS PAPAIOANNOU <63014049+PapaEvang01@users.noreply.github.com>
2025-06-21 14:14:03 +0300

    Update lab21b (2).asm

--- RISC-V_assembly/c_to_assembly/lab21b (2).asm
+++ RISC-V_assembly/c_to_assembly/lab21b (2).asm
@@ -1,59 +1,58 @@
-.data # data memory
-rows: .word 2
-cols: .word 3
+.data
+rows:       .word 2
+cols:       .word 3
+
 array2d:
-.word 342 # array[0][0]
-.word 654 # array[0][1]
-.word 263 # array[0][2]
-.word 463 # array[1][0]
-.word 975 # array[1][1]
-.word 378 # array[1][2]
+    .word 342, 654, 263
+    .word 463, 975, 378
+
 rows_sum:
-.word 0
-.word 0
+    .word 0, 0
 
-.text # program memory
+.text
+.globl main
 main:
-# pass arguments to function call:
-la a0, array2d # arg0 = input array address
-lw a1, cols # arg1 = number of columns
-lw a2, rows # arg2 = number of rows
-la a3, rows_sum # arg3 = output array address
-jal ra, compute_row_sum # function call
-j end_program
+    # Load arguments for function call
+    la   a0, array2d     # a0 = base address of 2D array
+    lw   a1, cols        # a1 = number of columns (M)
+    lw   a2, rows        # a2 = number of rows (N)
+    la   a3, rows_sum    # a3 = base address for output array
 
+    jal  ra, compute_row_sum
+
+    j end_program
+
+
+# void compute_row_sum(int a[N][M], int M, int N, int result[])
+# a0 = &a[0][0], a1 = M, a2 = N, a3 = result[]
 compute_row_sum:
-# a0 = start address of 2d array
-# a1 = number of columns
-# a2 = number of rows
-# a3 = start address of results array
+    slli s4, a1, 2       # s4 = column size in bytes (M * 4)
+    li   s1, 0           # i = 0
+    mv   s5, a0          # s5 = current row start address
 
+row_loop:
+    beq  s1, a2, end_rs  # if i == N, done
+    li   t0, 0           # sum = 0
+    li   s2, 0           # j = 0
+    mv   s6, s5          # s6 = current column address
 
-slli s0, a1, 2 # size of a single row
-li s1, 0 # int i = 0
+col_loop:
+    beq  s2, a1, end_sum
+    lw   t1, 0(s6)       # t1 = a[i][j]
+    add  t0, t0, t1      # sum += a[i][j]
+    addi s6, s6, 4       # move to next column
+    addi s2, s2, 1       # j++
+    j col_loop
 
-start_rs:
-beq s1, a2, end_rs # for (i=0; i<rows; i++)
-# pass arguments to function cal
-
-li t0, 0 # sum = 0
-
-add a1, a0, s0 # end address of array
-start_sum:
-beq a0, a1, end_sum
-lw t1, 0(a0) # load element
-add t0, t0, t1 # add to sum
-addi a0, a0, 4 # next array address
-j start_sum
 end_sum:
+    sw   t0, 0(a3)       # result[i] = sum
+    addi a3, a3, 4       # move to next result index
+    add  s5, s5, s4      # move to next row base address
+    addi s1, s1, 1       # i++
+    j row_loop
 
-sw t0,0(a3) #save to a0 stin proti thesi tou a3
-addi a3,a3,4 #proetoimazo gia thn deyterh thesh tou a3
-
-addi s1, s1, 1 # i++
-j start_rs
 end_rs:
-
-jr ra
+    jr ra
 
 end_program:
+    nop
